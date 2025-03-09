c5809d007db22c2c4891a4b9a5b8bbce2d0f721e
snowboundmage2 <snowboundmage2@gmail.com>
2025-03-09 14:18:43 +1000

    replaced with placeholders

--- wip/pc/resource/ResourceType.h
+++ wip/pc/resource/ResourceType.h
@@ -3,15 +3,15 @@
 
 namespace BK64 {
 enum class ResourceType {
-    Animation = 0x414E494D,
-    Binary = 0x43504C59,
-    DemoInput = 0x454E5653,
-    Dialog = 0x4C494D42,
-    GruntyQuestion = 0x4D534720,
-    LevelSetup = 0x4D534754,
-    Midi = 0x534B454C,
-    Model = 0x53435250,
-    QuizQuestion = 0x53434D44,
-    Sprite = 0x48544258
+    Animation = 0x00000000,
+    Binary = 0x00000000,
+    DemoInput = 0x00000000,
+    Dialog = 0x00000000,
+    GruntyQuestion = 0x00000000,
+    LevelSetup = 0x00000000,
+    Midi = 0x00000000,
+    Model = 0x00000000,
+    QuizQuestion = 0x00000000,
+    Sprite = 0x00000000
 };
 }
