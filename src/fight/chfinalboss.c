0e14c896fe773554e74fb13c4794ba503d2c4c9e
KiritoDv <kiritodev01@gmail.com>
2026-03-22 11:30:47 -0600

    Removed fake externs

--- src/fight/chfinalboss.c
+++ src/fight/chfinalboss.c
@@ -8,8 +8,6 @@
 extern void func_8028F4B8(f32[3], f32, f32);
 extern void func_80320ED8(ActorMarker *, f32, s32);
 s32 func_803297C8(Actor*, f32*);
-Actor *actor_spawnWithYaw_f32();
-extern Actor* marker_getActor(ActorMarker *this, s32 arg1);
 extern void actor_loopAnimation();
 extern void func_80324CFC(f32, enum comusic_e, s32);
 extern void sfxsource_setSampleRate(u8, s32);
