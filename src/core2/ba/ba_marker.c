e6b49f6d796aac84493b2e3685ac6f51cff70bb3
Jeod <47716344+JeodC@users.noreply.github.com>
2026-04-15 13:58:35 -0400

    Update ba_marker.c

--- src/core2/ba/ba_marker.c
+++ src/core2/ba/ba_marker.c
@@ -549,7 +549,7 @@ void __baMarker_resolveCollision(Prop *other_prop){
                         || (player_isStable() && !(3600.0f < ml_distanceSquared_vec3f(actor->position, spA0)))
                     ){
                         jiggyscore_setCollected(jiggy_id, true);
-                        BK_LOG_INFO("Banjo collected jiggy index %d in %s!", jiggy_id, port_mapName(map_get()));
+                        BK_LOG_INFO("Banjo collected jiggy index %d in %s!", jiggy_id, port_mapName(gsworld_getMap()));
                         item_adjustByDiffWithoutHud(ITEM_26_JIGGY_TOTAL, 1);
                         if(jiggy_id == JIGGY_20_BGS_ELEVATED_WALKWAY || jiggy_id == JIGGY_25_BGS_MAZE){
                             func_802D6924();
