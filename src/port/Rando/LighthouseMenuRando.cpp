968c7cdf5c1f6d70654358a1bd2c68db23adcf70
Douglas Simpson <scorched11@gmail.com>
2026-07-10 09:19:51 -0500

    Added functionality to shuffle honeycombs from beehives

--- src/port/Rando/LighthouseMenuRando.cpp
+++ src/port/Rando/LighthouseMenuRando.cpp
@@ -127,6 +127,9 @@ void LighthouseMenu::AddMenuRando() {
 
     AddWidget(path, "Advanced Shuffles", WIDGET_SEPARATOR_TEXT);
 
+    AddWidget(path, "Shuffle Beehives", WIDGET_CVAR_CHECKBOX)
+        .CVar(Rando::StaticData::Options[RO_SHUFFLE_BEEHIVE_HONEYCOMBS].cvar)
+        .Options(CheckboxOptions().Tooltip("Shuffles Contents of Beehives into the Pool."));
     AddWidget(path, "Shuffle Blue Eggs", WIDGET_CVAR_CHECKBOX)
         .CVar(Rando::StaticData::Options[RO_SHUFFLE_BLUE_EGGS].cvar)
         .Options(CheckboxOptions().Tooltip("Shuffles Blue Eggs into the Pool."));
