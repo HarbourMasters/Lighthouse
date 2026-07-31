47e5a3943b383d13e6cb1ce1e6b83d68212b156d
Malkierian <malkierian@live.com>
2026-07-31 09:45:52 -0700

    More and better guards.

--- src/core2/actor_cubepropsystem.c
+++ src/core2/actor_cubepropsystem.c
@@ -520,20 +520,20 @@ s32 func_8032D9C0(Cube *cube, Prop* prop){
     s32 tmp;
 
     sp24 = 0;
-    if(cube != NULL && cube->prop2Cnt != 0 && prop != NULL){
+    if(cube != NULL && cube->prop2Cnt != 0 && prop != NULL && cube->prop2Ptr != NULL){
         sp24 = prop->unk8_1;
-        if(func_80305D14()){
-            func_80305CD8(func_803058C0(prop->unk4[1]), -1);
-        }
-
-        // port - Rando manipulation of prop2Ptr results in mismatched data, this corrects that.
         ptrdiff_t index = prop - cube->prop2Ptr;
-        ptrdiff_t elementsAfter = (cube->prop2Cnt - 1) - index;
         // [port] A stale propPtr makes elementsAfter enormous; drop the removal instead.
         if (index < 0 || index >= (ptrdiff_t)cube->prop2Cnt) {
             port_warnPropNotInCube((s32)index, (s32)cube->prop2Cnt);
             return sp24;
         }
+        if(func_80305D14()){
+            func_80305CD8(func_803058C0(prop->unk4[1]), -1);
+        }
+
+        // port - Rando manipulation of prop2Ptr results in mismatched data, this corrects that.
+        ptrdiff_t elementsAfter = (cube->prop2Cnt - 1) - index;
         if (elementsAfter > 0) {
             memmove(prop, prop + 1, elementsAfter * sizeof(Prop));
         }
