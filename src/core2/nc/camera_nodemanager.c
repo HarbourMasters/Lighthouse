ef9170e9a448cd463deebce0c7f80d4f635f92b1
Jeod <47716344+JeodC@users.noreply.github.com>
2026-08-02 15:06:31 -0400

    Restore rumble to proper state and restore screenshake

--- src/core2/nc/camera_nodemanager.c
+++ src/core2/nc/camera_nodemanager.c
@@ -307,6 +307,7 @@ void ncCamera_update(void){
     viewport_getPosition_vec3f(sp2C);
     viewport_getRotation_vec3f(sp20);
     func_802BB4D8(sp2C, sp20);
+    viewport_setPosition_vec3f(sp2C);
     viewport_update();
     func_802BEFB0();
     func_802BBA84();
