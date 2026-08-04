ec1635de4eeeb8de28c4cdd64f3bb50febf70eee
Paco Avelar <[email protected]>
2026-08-04 06:50:46 +0000

    fix(android): use safe ImGui timer format string

--- src/port/Enhancements/Trackers/DisplayOverlay.cpp
+++ src/port/Enhancements/Trackers/DisplayOverlay.cpp
@@ -68,7 +68,7 @@ void DisplayOverlayWindow::Draw() {
             break;
     }
     std::string timerStr = port_FormatTimeDisplay(timeToDisplay);
-    ImGui::Text(timerStr.c_str());
+    ImGui::Text("%s", timerStr.c_str());
 
     ImGui::End();
 
