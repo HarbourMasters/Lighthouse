04a92efe607284ba78bdfa354b1f267d1ef28360
Jeod <47716344+JeodC@users.noreply.github.com>
2026-08-07 08:59:06 -0400

    Use RANDO_SAVE_CHECKS alias in Switch.cpp

--- src/port/Rando/ObjectBehavior/Switch.cpp
+++ src/port/Rando/ObjectBehavior/Switch.cpp
@@ -42,7 +42,7 @@ void Rando::ObjectBehavior::ModifySwitchBehavior(int32_t switchActorId) {
     }
 
     // Don't play missed switch scene if the check's been obtained
-    if (Rando::Logic::GetShuffledObject(randoCheckId).obtained) {
+    if (RANDO_SAVE_CHECKS[randoCheckId].obtained) {
         return;
     }
 
